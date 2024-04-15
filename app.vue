dea43cfdc0a31293bd330097384a02a697114914
hyeon9782 <hyeon9782@gmail.com>
2024-04-15 22:35:32 +0900

    feat: nuxt.js routing 학습

--- app.vue
+++ app.vue
@@ -1,3 +1,16 @@
 <template>
-  <div class="text-3xl font-bold underline">Hello Nuxt</div>
+  <div class="text-3xl font-bold underline">
+    <NuxtLayout>
+      <header>
+        <ul>
+          <li>
+            <NuxtLink to="/">Home</NuxtLink>
+            <NuxtLink to="/events">Events</NuxtLink>
+            <NuxtLink to="/events/profile">Profile</NuxtLink>
+          </li>
+        </ul>
+      </header>
+      <NuxtPage />
+    </NuxtLayout>
+  </div>
 </template>
---
+++ pages/events/[id].vue
@@ -0,0 +1,10 @@
+<script setup>
+const nuxt = useNuxtApp();
+const route = useRoute();
+console.log(nuxt);
+console.log(route.params.id);
+</script>
+
+<template>
+  <div>event id: {{ $route.params.id }}</div>
+</template>
